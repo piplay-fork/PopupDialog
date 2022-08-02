ebbbd3feba07be6d06c3228471d10e7c2f40473e
paynechu <314176+paynechu@users.noreply.github.com>
2022-08-02 16:06:33 +0800

    - improved the way to get key window again

--- PopupDialog/Classes/PopupDialog.swift
+++ PopupDialog/Classes/PopupDialog.swift
@@ -360,18 +360,8 @@ extension PopupDialog {
 
 extension PopupDialog {
     
-    func getKeyWindow() -> UIWindow {
-        if #available(iOS 15.0, *) {
-            return ((UIApplication.shared.connectedScenes.first(where: {$0.isKind(of: UIWindowScene.self)}) as? UIWindowScene)?.windows.first(where: {$0.canBecomeKey}))!
-        } else if #available(iOS 13.0, *) {
-            return (UIApplication.shared.windows.first(where: { $0.isKeyWindow }))!
-        } else {
-            return UIApplication.shared.keyWindow!
-        }
-    }
-    
     /// Present dialog by root view controller
     @objc public func show(animated: Bool = true, completion: (() -> Void)? = nil) {
-        self.getKeyWindow().rootViewController!.present(self, animated: animated, completion: completion)
+        (UIApplication.shared.delegate?.window)!!.rootViewController!.present(self, animated: animated, completion: completion)
     }
 }
